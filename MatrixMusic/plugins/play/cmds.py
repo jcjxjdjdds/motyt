474bb8650b605daaa7066726ae2d058819db418c
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-25 14:14:40 +0200

    Update cmds.py

--- MatrixMusic/plugins/play/cmds.py
+++ MatrixMusic/plugins/play/cmds.py
@@ -16,7 +16,7 @@ async def zdatsr(client: Client, message: Message):
     name = usr.first_name
     usrnam = usr.username
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/e15b16465f42229456e72.jpg",
+        photo=f"https://telegra.ph/file/4243b80b89cb8f9ffb4a4.jpg",
         caption=f"""<b>↯︙مرحباً بك عزيزي</b>\n<b>↯︙استخدم الازرار بالاسفل\n» ل تصفح اوامر الميوزك</b>""",
         reply_markup=InlineKeyboardMarkup(
             [
