114f06f5de227a719c9b17e5cff3e045169c7fd4
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-02-28 15:21:03 +0200

    Update ahm.py

--- MatrixMusic/plugins/ahm.py
+++ MatrixMusic/plugins/ahm.py
@@ -1,75 +1,79 @@
-from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import os
+from AnonX import app
 
-matrixes = {}
-@app.on_message(filters.reply & filters.regex("اهمس") & filters.group)
-def reply_with_link(client, message):
+hmses = {}
+
+@app.on_message(filters.reply & filters.regex("همسه") & filters.group)
+async def reply_with_link(client, message):
     user_id = message.reply_to_message.from_user.id
     my_id = message.from_user.id
     bar_id = message.chat.id
-    start_link = f"https://t.me/{(matrixpyrogram.get_me()).username}?start=matrix{my_id}to{user_id}in{bar_id}"
+    start_link = f"https://t.me/{(await app.get_me()).username}?start=hms{my_id}to{user_id}in{bar_id}"
     reply_markup = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("‹ اضغط هنا ›", url=start_link)]
+            [InlineKeyboardButton("- اضغط لإرسال الهمسه!", url=start_link)]
         ]
     )
-    message.reply_text("↯︙اضـغط هنا ↫ ⦗ لإرسال الهمسة السرية ⦘", reply_markup=reply_markup)
-waiting_for_matrix = False
-@app.on_message(filters.command("start"))
-def matrix_start(client, message):
-  
-  if message.text.split(" ", 1)[-1].startswith("matrix"):
-    global waiting_for_matrix, matrix_ids
-    matrix_ids = message.text
-    waiting_for_matrix = True
-    message.reply_text(
-      "↯︙ارسل رسالتك",
+    await message.reply_text("\n╢ إضغط لإرسال همسه!\n", reply_markup=reply_markup, reply_to_message_id=message.message_id)
+
+waiting_for_hms = False
+@app.on_message(filters.command("start"), group=473)
+async def hms_start(client, message):
+  if message.text.split(" ", 1)[-1].startswith("hms"):
+    global waiting_for_hms, hms_ids
+    hms_ids = message.text
+    waiting_for_hms = True
+    await message.reply_text(
+      "-> أرسل الهمسه الآن.\n√",
       reply_markup = InlineKeyboardMarkup ([[
-        InlineKeyboardButton ("‹ إلغاء ›", callback_data="matrix_cancel")
+        InlineKeyboardButton ("إلغاء ❌️", callback_data="hms_cancel")
       ]])
     )
     return
-@app.on_message(filters.private & filters.text & ~filters.command("start"))
-def send_matrix(client, message):
-  
-  global waiting_for_matrix
-  if waiting_for_matrix:    
-    to_id = int(matrix_ids.split("to")[-1].split("in")[0])
-    from_id = int(matrix_ids.split("matrix")[-1].split("to")[0])
-    in_id = int(matrix_ids.split("in")[-1])
+
+@app.on_message(filters.private & filters.text & ~filters.command("start"), group=921)
+async def send_hms(client, message):
+  global waiting_for_hms
+  if waiting_for_hms:    
+    to_id = int(hms_ids.split("to")[-1].split("in")[0])
+    from_id = int(hms_ids.split("hms")[-1].split("to")[0])
+    in_id = int(hms_ids.split("in")[-1])
     to_url = f"tg://openmessage?user_id={to_id}"
     from_url = f"tg://openmessage?user_id={from_id}"
     
-    matrixes[str(to_id)] = { "matrix" : message.text, "bar" : in_id }
+    hmses[str(to_id)] = { "hms" : message.text, "bar" : in_id }
     
-    message.reply_text("↯︙تم إرسال الهمسة")
+    await message.reply_text("-> تم ارسال الهمسه.\n√")
     
-    matrixpyrogram.send_message(
+    await app.send_message(
       chat_id = in_id,
-      text = f"↯︙هذي الهمسة للحلو ↬ ⦗ {app.get_chat(to_id).first_name}]({to_url}) ⦘\n↯︙هو اللي يقدر يشوفها ↬ ⦗ [{app.get_chat(from_id).first_name}]({from_url}) ⦘",
-      reply_markup = InlineKeyboardMarkup ([[InlineKeyboardButton("‹ لرؤية الهمسة ›", callback_data = "matrix_answer")]])
+      text = f"╖ المستخدم [{(await app.get_chat(to_id)).first_name}]({to_url})\n╢ لديك همسه من البني آدم دا [{(await app.get_chat(from_id)).first_name}]({from_url})\n╜انت فقط من يستطيع رؤيتها 🔐",
+      reply_markup = InlineKeyboardMarkup ([[InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data = "hms_answer")]])
     )
     
-    waiting_for_matrix = False
+    waiting_for_hms = False
   
-@matrixpyrogram.on_callback_query(filters.regex("matrix_answer"))
-def display_matrix(client, callback):
-  
+@app.on_callback_query(filters.regex("hms_answer"))
+async def display_hms(client, callback):
   in_id = callback.message.chat.id
   who_id = callback.from_user.id
   
-  if matrixes.get(str(who_id)) is not None:
-    if matrixes.get(str(who_id))["bar"] == in_id:
-      callback.answer( matrixes.get(str(who_id))["matrix"], show_alert = True )
+  if hmses.get(str(who_id)) is not None:
+    if hmses.get(str(who_id))["bar"] == in_id:
+      await callback.answer( hmses.get(str(who_id))["hms"], show_alert = True )
   else:
-    callback.answer( "هذا الأمر لايخصك", show_alert = True )
+    await callback.answer( "بطل لعب ف حاجه مش بتاعتك يابابا 🗿", show_alert = True )
     
-@app.on_callback_query(filters.regex("matrix_cancel"))
-def cancel_matrix(client, callback):
+@app.on_callback_query(filters.regex("hms_cancel"))
+async def cancel_hms(client, callback):
   
-  global waiting_for_matrix
-  waiting_for_matrix = False
+  global waiting_for_hms
+  waiting_for_hms = False
   
-  matrixpyrogram.edit_message_text(
-  chat_id = callback.message.chat.id,
-  message_id = callback.message.id,
-  text = "↯︙تم الغاء ↫ ⦗ الهمسة ⦘")
+  await client.edit_message_text(
+      chat_id = callback.message.chat.id,
+      message_id = callback.message.id,
+      text = "-> تم إلغاء الهمسه!\n√")
+  
